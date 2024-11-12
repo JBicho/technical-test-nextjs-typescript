6163b5f560e3d7ae858e33cef3860484767c863a
João Bicho <joaorbicho@gmail.com>
2024-11-12 18:33:11 +0000

    fix: winston error in netlify

--- common/utils/logger.ts
+++ common/utils/logger.ts
@@ -1,18 +1,35 @@
 import winston from 'winston';
 
-// Won't add unit tests as winston is third party software
-export const logger = winston.createLogger({
+const logger = winston.createLogger({
   level: 'info',
   format: winston.format.combine(
-    winston.format.colorize(),
-    winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
-    winston.format.printf(({ timestamp, level, message }) => {
-      return `${timestamp} [${level}]: ${message}`;
-    })
+    winston.format.timestamp(),
+    winston.format.json()
   ),
   transports: [
-    new winston.transports.Console(),
-
-    new winston.transports.File({ filename: 'logs/app.log' }),
+    new winston.transports.Console({
+      format: winston.format.combine(
+        winston.format.colorize(),
+        winston.format.simple()
+      ),
+    }),
   ],
 });
+
+if (process.env.NODE_ENV !== 'production') {
+  logger.add(
+    new winston.transports.File({
+      filename: 'logs/error.log',
+      level: 'error',
+      handleExceptions: true,
+    })
+  );
+  logger.add(
+    new winston.transports.File({
+      filename: 'logs/combined.log',
+      handleExceptions: true,
+    })
+  );
+}
+
+export { logger };
