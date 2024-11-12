071aa135bafcd694cfbfa8221f4d6d4cdf0f129e
João Bicho <joaorbicho@gmail.com>
2024-11-12 19:11:30 +0000

    fix: build issue with winston logger

--- common/utils/logger.ts
+++ common/utils/logger.ts
@@ -1,35 +1,49 @@
 import winston from 'winston';
 
-const logger = winston.createLogger({
-  level: 'info',
-  format: winston.format.combine(
-    winston.format.timestamp(),
-    winston.format.json()
-  ),
-  transports: [
-    new winston.transports.Console({
+const createLogger = () => {
+  if (typeof window !== 'undefined') {
+    return winston.createLogger({
+      level: 'info',
       format: winston.format.combine(
-        winston.format.colorize(),
-        winston.format.simple()
+        winston.format.timestamp(),
+        winston.format.json()
       ),
-    }),
-  ],
-});
+      transports: [
+        new winston.transports.Console({
+          format: winston.format.simple(),
+        }),
+      ],
+    });
+  }
 
-if (process.env.NODE_ENV !== 'production') {
-  logger.add(
-    new winston.transports.File({
-      filename: 'logs/error.log',
-      level: 'error',
-      handleExceptions: true,
-    })
-  );
-  logger.add(
-    new winston.transports.File({
-      filename: 'logs/combined.log',
-      handleExceptions: true,
-    })
-  );
-}
+  const logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.combine(
+      winston.format.timestamp(),
+      winston.format.json()
+    ),
+    transports: [
+      new winston.transports.Console({
+        format: winston.format.simple(),
+      }),
+    ],
+  });
 
-export { logger };
+  if (process.env.NODE_ENV === 'development') {
+    logger.add(
+      new winston.transports.File({
+        filename: 'logs/error.log',
+        level: 'error',
+      })
+    );
+    logger.add(
+      new winston.transports.File({
+        filename: 'logs/combined.log',
+      })
+    );
+  }
+
+  return logger;
+};
+
+export const logger = createLogger();
